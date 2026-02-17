e28d9a91ec020d6129305dea20f1457532789ddd
Geeky Lynx <73647896+geeky-lynx@users.noreply.github.com>
2026-02-17 17:55:34 +0100

    Client: Refactor main loop for readibility

--- OsProcesi/Client/Program.cs
+++ OsProcesi/Client/Program.cs
@@ -7,15 +7,18 @@ namespace Client
 {
     public class Client
     {
+        private static bool isLoggedIn = false;
+        private static bool isRunning = true;
+        private static Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        private static IPEndPoint destinationEp = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 50_001);
+        private static EndPoint receiverEp = new IPEndPoint(IPAddress.None, 0);
+        private static string loggedInUsername = "";
+
+
         public static void Main(string[] args)
         {
             Console.WriteLine("[Client]: Hello, World!");
 
-            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            IPEndPoint destinationEp = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 50_001);
-            EndPoint receiverEp = new IPEndPoint(IPAddress.None, 0);
-            bool isRunning = true;
-            bool isLoggedIn = false;
 
             Console.WriteLine("Commands:");
             Console.WriteLine("\tlogin (username) (password)");
@@ -32,7 +35,7 @@ namespace Client
             {
                 try
                 {
-                    Console.Write("> ");
+                    Console.Write($"{loggedInUsername}> ");
                     string? command = Console.ReadLine();
 
                     // Validate user input
@@ -51,56 +54,9 @@ namespace Client
 
                     string commandName = parts[0].ToLower();
                     if (commandName.Equals("login"))
-                    {
-                        if (isLoggedIn)
-                        {
-                            Console.WriteLine("Already logged in. Try other command");
-                            continue;
-                        }
-
-                        byte[] msg = Encoding.UTF8.GetBytes(command);
-
-                        byte[] responseByte = new byte[1024];
-                        int bytesCount = -1;
-                        if (!isLoggedIn)
-                            bytesCount = clientSocket.SendTo(msg, msg.Length, SocketFlags.None, destinationEp);
-                        else
-                            bytesCount = clientSocket.Send(msg);
-                        Console.WriteLine($"Sent {bytesCount} bytes");
-
-                        int receivedBytes = clientSocket.ReceiveFrom(responseByte, 1024, SocketFlags.None, ref receiverEp);
-                        string received = Encoding.UTF8.GetString(responseByte);
-
-                        Console.WriteLine($"Got a message (len = {receivedBytes}) from {receiverEp}.");
-                        Console.WriteLine($"Message received:");
-                        Console.WriteLine(received);
-
-                        if (!received.Contains("Success"))
-                        {
-                            Console.WriteLine("Server returned error message.");
-                            continue;
-                        }
-
-                        int port = -1;
-                        bool portSuccess = int.TryParse(received.Split('=')[1], out port);
-                        if (!portSuccess)
-                        {
-                            Console.WriteLine("Failed to parse the received port.");
-                            continue;
-                        }
-
-                        // Connect to the new given Tcp socket (with a new port)
-                        clientSocket.Close();
-                        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                        destinationEp.Port = port;
-                        clientSocket.Connect(destinationEp);
-                        isLoggedIn = true;
-                    }
+                        CommandLogin(command);
                     else
-                    {
                         Console.WriteLine($"Command \'{commandName}\' doesn\'t exist");
-                    }
-
                 }
                 catch (SocketException exception)
                 {
@@ -134,5 +90,63 @@ namespace Client
         //    }
         //    return true;
         //}
+
+
+
+        private static void CommandLogin(string command)
+        {
+            if (isLoggedIn)
+            {
+                Console.WriteLine("Already logged in. Try other command");
+                return;
+            }
+
+            string[] parts = command.Split(' ');
+            if (parts.Length != 3)
+            {
+                Console.WriteLine($"Error: `login` command expexts 2 arguments, but {parts.Length - 1} is given");
+                Console.WriteLine("Info: Format: `login (username) (password)`");
+                return;
+            }
+
+            byte[] msg = Encoding.UTF8.GetBytes(command);
+
+            byte[] responseByte = new byte[1024];
+            int bytesCount = -1;
+            if (!isLoggedIn)
+                bytesCount = clientSocket.SendTo(msg, msg.Length, SocketFlags.None, destinationEp);
+            else
+                bytesCount = clientSocket.Send(msg);
+            Console.WriteLine($"Sent {bytesCount} bytes");
+
+            int receivedBytes = clientSocket.ReceiveFrom(responseByte, 1024, SocketFlags.None, ref receiverEp);
+            string received = Encoding.UTF8.GetString(responseByte);
+
+            Console.WriteLine($"Got a message (len = {receivedBytes}) from {receiverEp}.");
+            Console.WriteLine($"Message received:");
+            Console.WriteLine(received);
+
+            if (!received.Contains("Success"))
+            {
+                Console.WriteLine("Server returned error message.");
+                return;
+            }
+
+            int port = -1;
+            bool portSuccess = int.TryParse(received.Split('=')[1], out port);
+            if (!portSuccess)
+            {
+                Console.WriteLine("Failed to parse the received port.");
+                return;
+            }
+
+            // Connect to the new given Tcp socket (with a new port)
+            clientSocket.Close();
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            destinationEp.Port = port;
+            clientSocket.Connect(destinationEp);
+            isLoggedIn = true;
+            loggedInUsername = parts[1];
+        }
     }
 }
